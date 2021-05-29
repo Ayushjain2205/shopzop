472b40fe0f2249ae8dd0cff32d58e7a17e6a8eff
LAPTOP-SNSEHSAR\ayush <ayushjain2205@gmail.com>
2021-05-29 18:09:02 +0530

    push files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { GraphQLClient } from "graphql-request";
+import "./App.css";
+
+import Shop from "./components/Shop/Shop";
+import Home from "./components/Home/Home";
 
 function App() {
+  const [shops, setShops] = useState(null);
+  const graphcms = new GraphQLClient(
+    "https://api-ap-northeast-1.graphcms.com/v2/ckp8dwi04vju901xp03oz89yu/master"
+  );
+  useEffect(() => {
+    const fetchShops = async () => {
+      const { shops } = await graphcms.request(
+        `
+      { 
+        shops {
+          name
+          rating
+          slug
+          category
+          logo{
+            id
+            handle
+          }
+          shopPicture{
+            id
+            handle
+          }
+          description{
+            html
+          }
+          googleLink
+          websiteLink
+          products{
+            name
+            price
+            image{
+              id
+              handle
+            }
+          }
+        }
+      }
+    `
+      );
+
+      setShops(shops);
+    };
+
+    fetchShops();
+    console.log(shops);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        {!shops ? (
+          "Loading"
+        ) : (
+          <Switch>
+            <Route exact path='/' render={(props) => <Home shops={shops} />} />
+            <Route path='/shops/:slug'>
+              <Shop shops={shops} />
+            </Route>
+          </Switch>
+        )}
+      </Router>
     </div>
   );
 }
